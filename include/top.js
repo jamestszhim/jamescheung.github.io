98e8a1302390d11198ab91dfb6f873c5898d9f8f
James <jamescheung121@gmail.com>
2016-06-15 10:16:35 +0800

    Initial commit

---
+++ include/top.js
@@ -0,0 +1,45 @@
+document.write('\
+<!-- Navigation -->\
+    <nav class="navbar navbar-default navbar-fixed-top">\
+        <div class="container">\
+            <div class="navbar-header page-scroll">\
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">\
+                <span class="sr-only">Toggle navigation</span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+            </button>\
+                <a class="navbar-brand page-scroll" href="#page-top">\
+                    <i class="fa fa-terminal"></i>James Cheung\
+                </a>\
+            </div>\
+\
+            <!-- Collect the nav links, forms, and other content for toggling -->\
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+                <ul class="nav navbar-nav navbar-right">\
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->\
+                    <li class="hidden">\
+                        <a href="#page-top"></a>\
+                    </li>\
+                    <li>\
+                        <a href="index.html">Home</a>\
+                    </li>\
+                    <li>\
+                        <a href="project.html">Project</a>\
+                    </li>\
+                    <li>\
+                        <a href="about.html">About</a>\
+                    </li>\
+                    <li>\
+                        <a href="experience.html">Experience</a>\
+                    </li>\
+                    <li>\
+                        <a href="contact.html">Contact</a>\
+                    </li>\
+                </ul>\
+            </div>\
+            <!-- /.navbar-collapse -->\
+        </div>\
+        <!-- /.container -->\
+    </nav>\
+');
